aaf2512004553aa321f458c0b3c0416b01811642
Garrett Hale <gthkeno@gmail.com>
2017-07-26 20:54:30 -0500

    javascript loaded guide

---
+++ guideData.js
@@ -0,0 +1,15 @@
+var guidedb = [ "SOCIAL", "index.html", "ABOUT", "about.html" ];
+
+function guide_load()
+{
+	var tag_guide = document.getElementById( "guide" );
+	var tag_center = document.createElement( "center" );
+	tag_guide.appendChild( tag_center );
+	for( i = 0; i < guidedb.length/2; i++ )
+	{
+		var tag_a = document.createElement( "a" );
+		tag_a.innerHTML = guidedb[i*2+0];
+		tag_a.href = guidedb[i*2+1];
+		tag_center.appendChild( tag_a );
+	}
+}
