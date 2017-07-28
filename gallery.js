27a7dae346dcb1a0d9d9164846f5556fcb52faeb
Garrett Hale <gthkeno@gmail.com>
2017-07-28 17:20:21 -0500

    gallery database looks nicer

--- gallery.js
+++ gallery.js
@@ -1,13 +1,13 @@
 var database = [
-	"Salt", "http://i.imgur.com/zbs5K8L.jpg",
-	"Mental Infrastructure", "http://i.imgur.com/mCDFIkQ.jpg",
-	"Fluid", "http://i.imgur.com/9y26TRy.jpg",
-	"Sketch 1", "http://i.imgur.com/6P2QsQf.jpg",
-	"BlueJay", "http://i.imgur.com/Cykwunr.jpg",
-	"Elephant Walking", "http://i.imgur.com/cj5HZi6.jpg",
-	"Swirl", "http://i.imgur.com/QamRccm.jpg",
-	"The Other Side", "http://i.imgur.com/A0ZoXLI.jpg",
-	"Special Tea", "http://i.imgur.com/wqkYhYL.jpg",
+	"Salt",                     "http://i.imgur.com/zbs5K8L.jpg",
+	"Mental Infrastructure",    "http://i.imgur.com/mCDFIkQ.jpg",
+	"Fluid",                    "http://i.imgur.com/9y26TRy.jpg",
+	"Sketch 1",                 "http://i.imgur.com/6P2QsQf.jpg",
+	"BlueJay",                  "http://i.imgur.com/Cykwunr.jpg",
+	"Elephant Walking",         "http://i.imgur.com/cj5HZi6.jpg",
+	"Swirl",                    "http://i.imgur.com/QamRccm.jpg",
+	"The Other Side",           "http://i.imgur.com/A0ZoXLI.jpg",
+	"Special Tea",              "http://i.imgur.com/wqkYhYL.jpg",
 	"Openning Your Second Eye", "http://i.imgur.com/U8KIc7u.jpg",
 ];
 
